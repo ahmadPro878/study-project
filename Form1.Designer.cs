ea85b10526efa3440fedd50520fcde9af9ca7065
ahmadPro878 <45334971+ahmadPro878@users.noreply.github.com>
2018-11-25 17:16:03 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 31);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(466, 176);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(844, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 82);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "READ GRAPH (from text)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(844, 121);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(247, 93);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Get Incedence";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(649, 31);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 87);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Adjacency linked list ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(649, 127);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 30);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(844, 236);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 125);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Load and Display the adjaceny linkedList";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(844, 368);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button5.Size = new System.Drawing.Size(224, 73);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Adj linkedList";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(970, 390);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(83, 30);
+            this.textBox2.TabIndex = 7;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(13, 236);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(466, 180);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(649, 236);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(131, 62);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "BFS";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(708, 252);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 30);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 783);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       // bool[] visted;
+     //   Stack<int> stack1; 
+        string[] EndVertex1, EndVertex2, AllVertex;
+        int NumOfEdges, NumofVertex;
+        List<string> linkedList = new List<string>();
+        int[,] IncedenceMatrix;
+
+        List<string> queueString=new List<string>();
+        List<int> queueInt = new List<int>();
+
+        List<string> final = new List<string>();
+
+        bool remove;
+
+        //capture child while serching BFS
+        List<string> process = new List<string>();
+        public void Read_Form_text()
+        {
+            string line;
+            string[] lineArray;
+            int counter = 0;
+            try
+            {
+                StreamReader sr = new StreamReader("Graph.txt") ; //don't forget "using System.IO;"
+                
+                
+                //Read all line to get the number of edge where eash line represent an edge
+                NumOfEdges = File.ReadAllLines("Graph.txt").Length - 1; //lenght-1 because the vertix line
+                //read the first line of the text
+                line = sr.ReadLine();   //here read the first line from the text
+                AllVertex = line.Split(':');
+                NumofVertex = AllVertex.Length;
+
+                EndVertex1 = new string[NumOfEdges];
+                EndVertex2 = new string[NumOfEdges];
+
+                line = sr.ReadLine(); //here he read the scound one,someway he incresed the read to the next line
+
+                while (line != null)
+                {
+                    lineArray = line.Split(',');
+                                          /* Trim() will removes all whitespace characters
+                                              * from the beginning and end of the string*/
+                    EndVertex1[counter] = lineArray[0].Trim(); 
+                    EndVertex2[counter] = lineArray[1].Trim();
+
+                    line = sr.ReadLine();
+                    counter++;
+                }
+                sr.Close();
+               
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception: " + e.Message);
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            Read_Form_text();
+            for (int i = 0; i < AllVertex.Length; i++)
+            {
+                richTextBox1.Text += AllVertex[i] + "\t";
+                
+            }
+            richTextBox1.Text += "\n";
+            for (int i = 0; i < EndVertex1.Length; i++)
+            {
+                richTextBox1.Text += EndVertex1[i] + "\t" + EndVertex2[i] + "\n";
+            }
+            
+
+        }
+        /*** GET INCEDENCE MATRIX ***/
+        void getInedence()
+        {
+            IncedenceMatrix = new int[NumofVertex, NumOfEdges];
+            for (int i = 0; i < EndVertex1.Length; i++)
+            {
+                /*because the matrix index start on "0" 
+              we decreases 1 from the given number of the text file*/
+                int end1 = Convert.ToInt32(EndVertex1[i]) - 1;
+                int end2 = Convert.ToInt32(EndVertex2[i]) - 1;
+                IncedenceMatrix[end1, i] = 1;
+                IncedenceMatrix[end2, i] = 1;
+            }
+        }
+
+        /*** GET ADJACENCY LINKED LIST ***/
+
+        public List<string> getAdjList(string node)
+        {
+            List<string> myList = new List<string>();
+            for (int i = 0; i < EndVertex1.Length; i++)
+            {
+                if (node == EndVertex1[i])
+                    if (!myList.Contains(EndVertex2[i]))//already added??!
+                    {
+                        myList.Add(EndVertex2[i]);
+                    }
+
+
+            }
+            for (int i = 0; i < EndVertex2.Length; i++)
+            {
+                if (node == EndVertex2[i])
+                    if (!myList.Contains(EndVertex1[i]))//already added??!
+                    {
+                        myList.Add(EndVertex1[i]);
+                    }
+            }
+            /*
+            for (int i = 0; i < EndVertex1.Length; i++)
+            {
+                if ((!(node == EndVertex1[i])) && (!(node == EndVertex2[i])))
+                    myList.Add("null");
+
+            }
+           */
+            return myList;
+
+        }
+        /*-------- GET INDEX------*/
+     //  int getIndex()
+     //   {
+
+
+      //  }
+
+        /*****BFS*****/
+        public void BFS(string s)
+        {
+            string temp;
+            // Mark all the vertices as not visited
+            bool[] visited = new bool[NumofVertex];
+            for (int i = 0; i < NumofVertex; i++)
+            {
+                visited[i] = false;
+            }
+
+            //Create a queue for BFS
+            
+            visited[0] = true;
+            queueString.Add(s);
+           
+
+            process = getAdjList(s); // we have the sun list,like 1-->5 2
+            for (int i = 0; i < process.Count; i++)
+            {
+                 queueString.Add(process.ElementAt(i)); 
+            }
+
+            /*-------- for tracker and display---------*/
+            richTextBox2.Text += ("process={");
+            for (int i = 0; i < process.Count; i++)
+            {
+                richTextBox2.Text += (process.ElementAt(i) + "\t");
+            }
+            richTextBox2.Text += ("}\n");
+
+            /*----------------------------------------*/
+            final.Add(queueString.ElementAt(0));
+            queueString.Remove(queueString.ElementAt(0));
+         
+            temp = queueString.ElementAt(0);
+            final.Add(queueString.ElementAt(0));
+            queueString.Remove(queueString.ElementAt(0));
+            
+            process = getAdjList(temp);
+            for (int i = 0; i < process.Count; i++)
+            {
+                queueString.Add(process.ElementAt(i));
+            }
+
+           
+            temp = queueString.ElementAt(0);
+            final.Add(queueString.ElementAt(0));
+            queueString.Remove(queueString.ElementAt(0));
+
+            process = getAdjList(temp);
+            for (int i = 0; i < process.Count; i++)
+            {
+                queueString.Add(process.ElementAt(i));
+            }
+
+         
+           
+
+
+            
+
+
+
+
+			// queueString.Add(process.ElementAt(0)); // we have 5 on queue ,bring her kinds 1 5
+            // queueString.Add(s);
+             
+           
+
+             visited[1]=   process.Remove(process.ElementAt(0)); /* remove 5 from the process
+                                                               * and mark this node as visited*/
+
+			
+            
+
+
+
+
+           
+            
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button6.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "Incedence Matrix is : \n";
+            getInedence();
+            for (int i = 0; i < NumofVertex; i++)
+            {
+                for (int j = 0; j < NumOfEdges; j++)
+                {
+                    richTextBox1.Text += IncedenceMatrix[i, j] + "\t";
+                }
+                richTextBox1.Text += "\n";
+                
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            List<string> list = new List<string>();
+            list = getAdjList(textBox1.Text);/* this list is the adjacency list*/
+
+           
+            richTextBox1.Text += "\n"+textBox1.Text+"\t";
+            for (int i = 0; i < list.Count; i++)
+            {
+                richTextBox1.Text += list.ElementAt(i) + " ";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+
+            for (int i = 0; i < NumofVertex; i++)
+            {
+                list = getAdjList(AllVertex.ElementAt(i));
+                
+                
+                richTextBox1.Text += "\n" + AllVertex.ElementAt(i) + "\t";
+                for (int j = 0; j < list.Count; j++)
+                {
+
+                    richTextBox1.Text += list.ElementAt(j) + " ";
+
+               //     linkedList.Add(list.ElementAt(j));
+                } 
+           //     linkedList.Add(" ");
+                //    list.Clear();
+            
+            }
+           /* tring to get all list element but there weap
+            foreach (string item in list)
+            {
+                richTextBox2.Text += item ;
+            }*/
+        }
+
+        //get the adjacncy vertix to the given one in textBox,using a list this time
+        private void button5_Click(object sender, EventArgs e)
+        {
+       //     richTextBox2.Text+=linkedList.ElementAt(Convert.ToInt32(textBox2.Text)+1);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string temp;
+            temp = textBox3.Text;
+            BFS(temp);
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            button6.Enabled = true;
+            
+        }
+    }
+}
